a4ed7d25f656ca76e4ca523ec98149c171bd177c
Xon <635541+Xon@users.noreply.github.com>
2026-01-10 03:19:19 +0800

    XF2.3 export

--- upload/src/addons/SV/UsernameRestrictions/_output/extension_hint.php
+++ upload/src/addons/SV/UsernameRestrictions/_output/extension_hint.php
@@ -3,6 +3,11 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+/**
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
 namespace SV\UsernameRestrictions\XF\Entity
 {
 	class XFCP_User extends \XF\Entity\User {}
---
+++ upload/src/addons/SV/UsernameRestrictions/_output/option_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $sv_ur_apply_to_admins Apply extra username restrictions to administrators
+ * @property bool|null $sv_ur_block_group_subset Block usernames as a subset of a user group name
+ */
+class Options
+{
+}
